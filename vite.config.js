55d5197616801e8f4fc879458a20aa47267d0637
Vishal Prajapati <kumar672399@gmail.com>
2025-06-04 17:53:02 +0530

    update data

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/Uniplay-web/',
+    base: 'http://localhost:5173/',
   plugins: [react()],
 })
